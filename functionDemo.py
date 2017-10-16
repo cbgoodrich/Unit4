db5bf7c1f75e97b24afdb8fd99cb94f5ef8bbbd2
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-16 14:22:11 -0400

    Updated from Brython Server: 10/16/2017 2:22:11 PM

--- functionDemo.py
+++ functionDemo.py
@@ -2,5 +2,15 @@
 #10/16/17
 #functionDemo.py - learning functions
 
-def hw():
-    print("Hello, world!")
+"""def hw():
+    print("Hello, world!")"""
+    
+def bigger(num1, num2): #prints which function is bigger
+    if num1 > num2:
+        print(num1)
+    else:
+        print(num2)
+        
+bigger(-13, -43)
+    
+
