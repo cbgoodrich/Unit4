e9057dbc94763f3c002ee601e7d521b6b384f904
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-16 14:42:20 -0400

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -2,9 +2,8 @@
 #10/16/17
 #countdown.py - prints the countdown
 
-def countdown():
-    n = int(input("Enter the number you want to countdown from: "))
+def countdown(n):
     for i in range(n, 0, -1):
         print(i)
     print("BOOM!")
-countdown()
+countdown(5)
