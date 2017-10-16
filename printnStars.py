d323347ac58b2dc0f3e562c80ed0f75081af099c
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-16 14:34:56 -0400

    Updated from Brython Server: 10/16/2017 2:34:56 PM

--- printnStars.py
+++ printnStars.py
@@ -1,1 +1,12 @@
+#Charlie Goodrich
+#10/16/17
+#printnStars.py - prints the number of stars
 
+def printnStars():
+    i = 1
+    n = int(input("Enter number of stars: "))
+    while i <= n:
+        print("*"*i)
+        i += 1
+
+printnStars()
