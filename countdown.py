408356cff6436bb415315e48638c7b60e455022f
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-16 14:37:55 -0400

    Updated from Brython Server: 10/16/2017 2:37:55 PM

--- countdown.py
+++ countdown.py
@@ -1,1 +1,10 @@
+#Charlie Goodrich
+#10/16/17
+#countdown.py - prints the countdown
 
+def countdown():
+    n = int(input("Enter the number you want to countdown from: "))
+    for i in range(n, 0, -1):
+        print(i)
+
+countdown()
