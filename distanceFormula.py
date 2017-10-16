290a18612c0930eefaf3655466bff78236db4520
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-16 15:13:21 -0400

    Updated from Brython Server: 10/16/2017 3:13:21 PM

--- distanceFormula.py
+++ distanceFormula.py
@@ -1,1 +1,11 @@
+#Charlie Goodrich
+#10/16/17
+#distanceFormula.py - distance formula between two points
 
+from math import sqrt
+
+def distance(x1, y1, x2, y2):
+    print(sqrt((x2-x1)**2 + (y2-y1)**2))
+    
+distance(0,0,3,4)
+    
