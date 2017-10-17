a1f020c57b28fc8498704c8514bd02c4dc0029df
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-17 14:41:10 -0400

    Updated from Brython Server: 10/17/2017 2:41:10 PM

--- printSquares.py
+++ printSquares.py
@@ -1,1 +1,8 @@
+#Charlie Goodrich
+#10/17/17
+#printSquares.py - prints a grid of squares
 
+def printSquares(rows, columns):
+    for j in range(columns):
+        for i in range(rows):
+            
