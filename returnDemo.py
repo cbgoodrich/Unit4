3614e0d892bbb4debcc0c593cfa2337e7f7bbe0a
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-18 13:37:47 -0400

    Updated from Brython Server: 10/18/2017 1:37:47 PM

--- returnDemo.py
+++ returnDemo.py
@@ -5,9 +5,12 @@
 from random import randint
 
 def randevenint(low, high):
-    return randint(low, high)
+    num = randint(low, high)
+    while num%2 == 1: #while the number is odd
+        num = randint(low, high)
+    return num
     
-    
+
 r1 = randevenint(1, 100)
 r2 = randevenint(1, 100)
 r3 = randevenint(1, 100)
