afd30ae029ef536653c68a2dc41551aa826667a7
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-18 20:47:11 -0400

    Updated from Brython Server: 10/18/2017 8:47:10 PM

--- stringUnion.py
+++ stringUnion.py
@@ -1,1 +1,14 @@
+#Charlie Goodrich
+#10/18/18
+#stringUnion.py - tells you the letters in two words
 
+def stringUnion(word1, word2):
+    total = " "
+    for ch in word1:
+        total += ch
+    for ch in word2:
+        total += ch
+        
+    print(total)
+    
+stringUnion("high", "kite")
