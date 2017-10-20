24acb1074a2ac9334a4b665256f8fde1bb5c4073
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-20 14:53:54 -0400

    Updated from Brython Server: 10/20/2017 2:53:54 PM

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,1 +1,18 @@
+#Charlie Goodrich
+#10/20/17
+#stringIntersect.py - prints out the letters in both strings
 
+def stringIntersect("word1", "word2"):
+    total = " "
+    for ch in word1:
+        if not ch in total:
+            total += ch
+            
+    for ch in word2:
+        if not ch in total:
+            total += ch
+    
+    return total
+
+total = stringUnion("mississippi", "pennsylvania")
+print(total)
