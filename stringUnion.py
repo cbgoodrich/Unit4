956cf3bd8030bba2985eb8033e3a8d39d4f9d6d4
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-20 14:51:43 -0400

    Updated from Brython Server: 10/20/2017 2:51:43 PM

--- stringUnion.py
+++ stringUnion.py
@@ -4,3 +4,15 @@
 
 def stringUnion(word1, word2):
     total = " "
+    for ch in word1:
+        if not ch in total:
+            total += ch
+            
+    for ch in word2:
+        if not ch in total:
+            total += ch
+    
+    return total
+
+total = stringUnion("mississippi", "pennsylvania")
+print(total)
