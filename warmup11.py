2d6b00c9a8eba45d4c7096b23604ca264890a50b
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-23 14:33:48 -0400

    Updated from Brython Server: 10/23/2017 2:33:47 PM

--- warmup11.py
+++ warmup11.py
@@ -1,1 +1,13 @@
+#Charlie Goodrich
+#10/23/17
+#warmup11.py - tells you if you've got a prime number
 
+def isPrime(number):
+    if number>2:
+        for i in range(2, number//2 + 1):
+            if number%i == 0:
+                return False
+        return True
+    else:
+        return True
+print(isPrime(9))
