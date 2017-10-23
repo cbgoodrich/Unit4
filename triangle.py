28b081298e1302daf58b4ba2935e44e28a18ac1f
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-23 14:48:05 -0400

    Updated from Brython Server: 10/23/2017 2:48:05 PM

--- triangle.py
+++ triangle.py
@@ -1,1 +1,17 @@
+#Charlie Goodrich
+#10/23/17
+#triangle.py - prints the area of a triangle
 
+from math import sqrt
+
+def triangle(x1, y1, x2, y2, x3, y3):
+    a = sqrt((x1-x2)**2 + (y1-y2)**2) #side a
+    b = sqrt((x2-x3)**2 + (y2-y3)**2) #side b
+    c = sqrt((x3-x1)**2 + (y3-y1)**2) #side c
+    
+    s = .5 * (a + b + C)
+    
+    area = sqrt(s*(s-a)*(s-b)*(s-c))
+    return area
+    
+print(triangle(1,2,3,4,5,6))
