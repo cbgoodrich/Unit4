60a2fbd2bfbe154ecf30ac739d847e72010fc2ca
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-24 13:42:28 -0400

    Updated from Brython Server: 10/24/2017 1:42:27 PM

--- multiply.py
+++ multiply.py
@@ -1,1 +1,21 @@
+#Charlie Goodrich
+#10/24/17
+#multiply.py - helps kids with multiplication
 
+from random import randint
+
+def message():
+    print("Watch out world. Here comes the next great multiplier!")
+
+correctCounter = 0
+while True:
+    num1 = randint(1, 12)
+    num2 = randint(1, 12)
+    print(num1, "x", num2, "=")
+    guess = int(input("? "))
+    ans = num1 * num2
+    if guess == ans:
+        correctCounter += 1
+    if guess == "stop":
+        break
+    
