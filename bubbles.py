15457cc49bfc9d57d4f0e5241a577374887f9e31
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-24 14:00:15 -0400

    Updated from Brython Server: 10/24/2017 2:00:13 PM

--- bubbles.py
+++ bubbles.py
@@ -1,1 +1,26 @@
+#Charlie Goodrich
+#10/24/17
+#bubbles.py - prints out lots of bubbles when you click
 
+from ggame import *
+from random import randint
+
+def mouseClick(event):
+    radius = randint(1, 100)
+    colorCode = randint(1, 3)
+    pos.x = randint(0, 700)
+    pos.y = randint(0, 500)
+
+    
+    if colorCode == 1:
+        red = Color(0xFF0000, 1)
+        bubble = CircleAsset(radius, LineStyle(1, red), red)
+    elif colorCode == 2:
+        green = Color(0x00FF00, 1)
+        bubble = CircleAsset(radius, LineStyle(1, green), green)
+    else:
+        blue = Color(0x0000FF, 1)
+        bubble = CircleAsset(radius, LineStyle(1, blue), blue)
+        
+        
+    
