e6ee72902f26ed20acb8d70ed8a15e19be2ebcf4
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-24 13:34:29 -0400

    Updated from Brython Server: 10/24/2017 1:34:27 PM

--- triangle.py
+++ triangle.py
@@ -4,6 +4,7 @@
 
 from math import sqrt
 
+#inputs
 x1 = float(input("x1 = "))
 y1 = float(input("y1 = "))
 x2 = float(input("x2 = "))
@@ -11,21 +12,22 @@ y2 = float(input("y2 = "))
 x3 = float(input("x3 = "))
 y3 = float(input("y3 = "))
 
+#finding the length of side a
 def distance_a(x1, y1, x2, y2):
-    a = (sqrt((x2-x1)**2 + (y2-y1)**2))
-    return a
+    return (sqrt((x2-x1)**2 + (y2-y1)**2))
 a = distance_a(x1, y1, x2, y2)
 
-def distance_b(x2, y2, x3, y3):
-    b = (sqrt((x3-x2)**2 + (y3-y2)**2))
-    return b
+#finding the length of side b
+def distance_b(x2, y2, x3, y3): 
+    return (sqrt((x3-x2)**2 + (y3-y2)**2))
 b = distance_b(x2, y2, x3, y3)
 
-def distance_c(x1, y1, x2, y2):
-    c = (sqrt((x1-x3)**2 + (y1-y3)**2))
-    return c
+#finding the length of side c
+def distance_c(x1, y1, x2, y2): 
+    return (sqrt((x1-x3)**2 + (y1-y3)**2))
 c = distance_c(x1, y1, x3, y3)    
 
+#finding s for computing the area
 s = .5 * (a + b + c)
 
 area = sqrt(s*(s-a)*(s-b)*(s-c))
