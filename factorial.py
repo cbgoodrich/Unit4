9d62ae768a68a34186d55a204b0b743e36bca46e
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-26 14:54:03 -0400

    Updated from Brython Server: 10/26/2017 2:54:03 PM

--- factorial.py
+++ factorial.py
@@ -1,1 +1,12 @@
+#Charlie Goodrich
+#10/26/17
+#factorial.py - a recursive way to do factorials
 
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print(factorial(5))
