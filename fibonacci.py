f781c78d0b1352dd7f815b6d522bca37e9dc370a
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-26 21:58:58 -0400

    Updated from Brython Server: 10/26/2017 9:58:57 PM

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,13 @@
+#Charlie Goodrich
+#10/26/17
+#fibonacci.py - prints the fibonacci sequence
 
+def fibonacci(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+        
+print(fibonacci(3))
