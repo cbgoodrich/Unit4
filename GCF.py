eafac7bb418bd1f8e1296e046d86485cc6878121
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-27 14:25:00 -0400

    Updated from Brython Server: 10/27/2017 2:24:58 PM

--- GCF.py
+++ GCF.py
@@ -1,1 +1,12 @@
+#Charlie Goodrich
+#10/27/17
+#GCF.py - prints the greatest common factor
 
+def GCF(num1, num2):
+    small = min(num1, num2)
+    for i in range(10, 0, -1):
+        if num1%i == 0 and num2%i == 0:
+            return i
+
+print(GCF(10,12))
+    
