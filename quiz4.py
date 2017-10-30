4c3a9c6c8c1189225ecdc02cc86f91277713e47f
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-30 14:27:18 -0400

    Updated from Brython Server: 10/30/2017 2:27:16 PM

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,30 @@
+#Charlie Goodrich
+#10/30/17
+"""quiz4.py - prints out CSIA four times, returns the average of 3 numbers
+prints the last letter of a word, returns True or False for two arguments"""
 
+def csia():
+    print("Computer Science is Awesome "*5)
+
+csia()
+
+def average(num1, num2, num3):
+    return (num1 + num2 + num3)/3
+    
+print(average(1,2,3))
+
+def lastLetter(word):
+    total = ""
+    for ch in word:
+        total += ch
+    print(total)
+        
+lastLetter("Charlie")
+
+
+def same(exp1, exp2):
+    if exp1 == exp2:
+        return True
+    else:
+        return False
+print(same(2*3, 7-1))
